7bd517cf89ddfadf453df14f4fcc00a4c4b4489f
Sarahgad <sarahgad91@gmail.com>
2024-07-12 23:40:42 +0300

    calc function with error handling

--- calc.js
+++ calc.js
@@ -1,2 +1,48 @@
 #!/user/bin/node
 
+function errorHandlingOpt(expression){
+    optArray = ['+','-','*','%','/','^']
+    count = 1;
+    for (i = 0; i< optArray.length; i++){
+        if (expression[0] != optArray[i]){
+            count++
+        }else{
+            continue;
+        }
+    }
+    if (count === optArray.length){
+        return `you should enter Argument first`
+    }
+return 0;
+}
+
+function errorHandlingArg(result){
+    if (result.length < 4){
+        return `Error please enter two argument at least`
+    }
+    return 0;
+}
+
+function calcWeb(result){
+    if (errorHandlingArg(result) != 0){
+            console.log(errorHandlingArg(result));
+            return;
+            }
+    expression = result.slice(2).join(' ')
+    if (errorHandlingOpt(expression) != 0){
+            console.log(errorHandlingOpt(expression));
+            return;
+    }
+    try{
+        finalResult = eval(expression)
+        if (finalResult === Infinity){
+            console.log('the result is meanless')
+        } else {
+    console.log(finalResult)
+        }
+    } catch(Error){
+        console.error(Error.message);
+    }
+}
+
+calcWeb(process.argv)
